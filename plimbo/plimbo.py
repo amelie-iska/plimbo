720fd8c97c6984a5e1bb05de16c560101929f451
leycec <leycec@gmail.com>
2018-12-13 14:50:37 -0500

    Top-level module added.
    The eponymous top-level "plimbo.plimbo" module has now been added as a
    short-term placeholder for more meaningful project structure.

---
+++ plimbo/plimbo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# --------------------( LICENSE                           )--------------------
+# Copyright 2018-2019 by Alexis Pietak & Cecil Curry.
+# See "LICENSE" for further details.
+
+'''
+Top-level Planarian Interface for Modelling Body Organization (pLIMBO) module.
+'''
+
+# ....................{ IMPORTS                           }....................
+import numpy as np
+
+# ....................{ CLASSES                           }....................
